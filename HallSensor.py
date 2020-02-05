390404f8d33f73ec48135be0f8a2bb3e0c577748
Michael <michael.p@fu-berlin.de>
2020-02-05 09:38:21 +0100

    bouncetime added

--- HallSensor.py
+++ HallSensor.py
@@ -2,6 +2,8 @@ import RPi.GPIO as GPIO
 
 class HallSensor:
 
+    DEBOUNCE = 500
+
     def __init__(self, hallPin, sensorCallback):
         #persist values
         self.hallPin = hallPin
@@ -11,7 +13,7 @@ class HallSensor:
         GPIO.setup(hallPin , GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
     def start(self):
-        GPIO.add_event_detect(self.hallPin, GPIO.BOTH, callback=self.sensorCallback, bouncetime=500)  
+        GPIO.add_event_detect(self.hallPin, GPIO.BOTH, callback=self.sensorCallback, bouncetime=self.DEBOUNCE)  
 
     def stop(self):
         GPIO.remove_event_detect(self.hallPin)
